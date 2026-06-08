411338455efaf18a701b31e4d39ecf6ea97eff37
Prakharr1804 <prakhar.porwal180406@gmail.com>
2026-06-08 14:42:26 +0530

    deleted

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import './style.scss'
 
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-const isConfigured = googleClientId && googleClientId !== 'your_google_client_id_here';
+const isConfigured = googleClientId && googleClientId !== '';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
